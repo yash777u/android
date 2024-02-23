a2346bdfc2a9d25fcdd98d6f0905dc0b9e62b068
yogesh <puli2004ad@gmail.com>
2024-02-23 22:31:36 +0530

    Login page associated with firebase

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,12 +10,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.android.gms.tasks.Task;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         Button loginButton = findViewById(R.id.login_login);
         EditText emailEditText = findViewById(R.id.email_login);
@@ -28,6 +40,8 @@ public class Login extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String pass = pass_login.getText().toString();
 
+
+                checkIfUserExists(email,pass);
                 if(email.isEmpty() || pass.isEmpty()) {
                     warningTextView.setText("Error - Fill All Details!!");
                 }
@@ -37,6 +51,26 @@ public class Login extends AppCompatActivity {
                     warningTextView.setText("Congratulations !!!!");
                 }
             }
+
+            public void checkIfUserExists(String email, String password) {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Login.this,"Firebase Login Sucessfull",Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, check if it's due to invalid user or invalid password
+                                    if (task.getException() instanceof FirebaseAuthInvalidUserException) {
+                                        Toast.makeText(Login.this,"User Do not Exist in Firebase",Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(Login.this,"Firebase INcorrect Password",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+            }
+
         });
     }
 }
